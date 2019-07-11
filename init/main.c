27eebc853359908b68b1f278a33ca6d843cc6e82
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 01:15:15 +0900

    dma_buf: use kmem_cache pool for struct dma_buf_attachment
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: John Vincent <git@tensevntysevn.cf>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- init/main.c
+++ init/main.c
@@ -738,6 +738,7 @@ void kdp_init(void)
 #endif /*CONFIG_RKP_KDP*/
 
 
+void __init init_dma_buf_kmem_pool(void);
 asmlinkage __visible void __init start_kernel(void)
 {
 	char *command_line;
@@ -960,6 +961,7 @@ asmlinkage __visible void __init start_kernel(void)
 	cgroup_init();
 	taskstats_init_early();
 	delayacct_init();
+	init_dma_buf_kmem_pool();
 
 
 	acpi_subsystem_init();
