9fac7de394d325efc3b5ea1411f3b25c516de992
igor121 <chernokalsky89@gmail.com>
2025-05-18 12:33:29 +0300

    Revert "schedutil: Allow CPU frequency changes to be amended before they're set"
    This reverts commit e0515459200d7430207694bf405f45080461cffa.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -218,9 +218,8 @@ static bool sugov_should_update_freq(struct sugov_policy *sg_policy, u64 time)
 	    !cpufreq_can_do_remote_dvfs(sg_policy->policy))
 		return false;
 
-	/* If the last frequency wasn't set yet then we can still amend it */
 	if (sg_policy->work_in_progress)
-		return true;
+		return false;
 
 	if (unlikely(sg_policy->need_freq_update)) {
 		sg_policy->need_freq_update = false;
